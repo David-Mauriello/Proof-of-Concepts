0773a706e8a52b07ac00138c2e488a6b00038833
David Mauriello <dmauriello82@gmail.com>
2021-07-16 09:45:57 -0400

    Initial commit

---
+++ src/test/java/com/example/featurecreep/FeatureCreepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.featurecreep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureCreepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
